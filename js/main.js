98f08e61dfb3d5935eb17664bc3b943d4b86b0ea
Alan Sánchez <Alan@MacBook-Air.local>
2015-11-02 22:45:38 -0600

    Add skills with graphs:
    	-Added some icon fonts
    	-Added jquery-circle-progress from kottenator (https://github.com/kottenator/jquery-circle-progress)

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+$('.skill-html5').circleProgress({
+    value: 0.85,
+    size: 250,
+    thickness: 30,
+    fill: {
+        color: "#E44D26"
+    }
+});
+$('.skill-css3').circleProgress({
+    value: 0.75,
+    size: 250,
+    thickness: 30,
+    fill: {
+        color: "#0171BB"
+    }
+});
+$('.skill-js').circleProgress({
+    value: 0.20,
+    size: 250,
+    thickness: 30,
+    fill: {
+        color: "#E5A32B"
+    }
+});
+$('.skill-php').circleProgress({
+    value: 0.35,
+    size: 250,
+    thickness: 30,
+    fill: {
+        color: "#6785B7"
+    }
+});
+$('.skill-git').circleProgress({
+    value: 0.40,
+    size: 250,
+    thickness: 30,
+    fill: {
+        color: "#F34F29"
+    }
+});
