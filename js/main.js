efce396bb8c35d6c5a0f8508e245520a40d99380
Alan Sánchez <Alan@MacBook-Air.local>
2015-11-04 16:46:43 -0600

    Add jquery.appear.js Skills animations now fire up when visible on the viewport

--- js/main.js
+++ js/main.js
@@ -24,45 +24,63 @@ $('#projects ul li').mouseenter(function() {
     $(this).find('a').removeClass('mouseover');
 });
 
-/* Circle graphs */
-$('.skill-html5').circleProgress({
-    value: 0.85,
-    size: 250,
-    thickness: 30,
-    fill: {
-        color: "#E44D26"
-    }
-});
-$('.skill-css3').circleProgress({
-    value: 0.75,
-    size: 250,
-    thickness: 30,
-    fill: {
-        color: "#0171BB"
-    }
-});
-$('.skill-js').circleProgress({
-    value: 0.20,
-    size: 250,
-    thickness: 30,
-    fill: {
-        color: "#E5A32B"
-    }
-});
-$('.skill-php').circleProgress({
-    value: 0.35,
-    size: 250,
-    thickness: 30,
-    fill: {
-        color: "#6785B7"
-    }
-});
-$('.skill-git').circleProgress({
-    value: 0.40,
-    size: 250,
-    thickness: 30,
-    fill: {
-        color: "#F34F29"
-    }
+
+
+
+/* Skills */
+
+
+var el = $('#skills'),
+    inited = false;
+
+el.appear({ force_process: true });
+
+el.on('appear', function() {
+  if (!inited) {
+
+    $('.skill-html5').circleProgress({
+        value: 0.85,
+        size: 250,
+        thickness: 30,
+        fill: {
+            color: "#E44D26"
+        }
+    });
+    $('.skill-css3').circleProgress({
+        value: 0.75,
+        size: 250,
+        thickness: 30,
+        fill: {
+            color: "#0171BB"
+        }
+    });
+    $('.skill-js').circleProgress({
+        value: 0.20,
+        size: 250,
+        thickness: 30,
+        fill: {
+            color: "#E5A32B"
+        }
+    });
+    $('.skill-php').circleProgress({
+        value: 0.35,
+        size: 250,
+        thickness: 30,
+        fill: {
+            color: "#6785B7"
+        }
+    });
+    $('.skill-git').circleProgress({
+        value: 0.40,
+        size: 250,
+        thickness: 30,
+        fill: {
+            color: "#F34F29"
+        }
+    });
+
+
+    inited = true;
+  }
 });
 
