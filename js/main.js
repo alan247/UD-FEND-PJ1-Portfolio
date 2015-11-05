3e10cdd0b2a693c2a850191fea5039150e636653
Alan Sánchez <Alan@MacBook-Air.local>
2015-11-04 21:44:02 -0600

    Check and clean code. Validate.
    	-W3 HTML Validator shows some warnings related to the use of sections without headings. I considered changing all <sections> to <divs> but figured that it would be like going back in time. Besides, it's just a recommendation, not an error.
    
    	-W3 CSS Validator shows 2 errors regarding the use of calc(). Apparently it is a validator bug, as explained here https://www.w3.org/Bugs/Public/show_bug.cgi?id=18913

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-
-
-
 /* Menu */
 $(window).scroll(function() {
     if ($('main').position().top - $(window).scrollTop() > -706) {
@@ -23,90 +20,78 @@ $(window).scroll(function() {
 
 
 /* Type text animation */
-
-$('.welcome-text').empty().typetype(
-    'var hello = "Hi, I\'m Alan, a front-end developer";',
-    {
-        e: 0.03,
-        t: 80
+$('.welcome-text').empty().typetype('var hello = "Hi, I\'m Alan, a front-end developer";', {
+    e: 0.03,
+    t: 80
+}).delay(800).backspace(51, {
+    t: 20,
+    callback: function() {
+        $('.welcome-text').addClass('take-a-look');
     }
- ).delay(1500).backspace(
-    51,
-    {
-        t: 20,
-        callback: function(){
-           $('.welcome-text').addClass('take-a-look');
-        }
-    }).typetype('TAKE A LOOK AROUND!');
-
-
+}).typetype('TAKE A LOOK AROUND!');
 
 
 /* Projects */
-
 $('#projects ul li').mouseenter(function() {
     $(this).find('a').addClass('mouseover');
-}).mouseleave(function(){
+}).mouseleave(function() {
     $(this).find('a').removeClass('mouseover');
 });
 
 
-
-
 /* Skills */
-
-
 var el = $('#skills'),
     inited = false;
 
-el.appear({ force_process: true });
-
-el.on('appear', function() {
-  if (!inited) {
-
-    $('.skill-html5').circleProgress({
-        value: 0.85,
-        size: 250,
-        thickness: 30,
-        fill: {
-            color: "#E44D26"
-        }
-    });
-    $('.skill-css3').circleProgress({
-        value: 0.75,
-        size: 250,
-        thickness: 30,
-        fill: {
-            color: "#0171BB"
-        }
-    });
-    $('.skill-js').circleProgress({
-        value: 0.20,
-        size: 250,
-        thickness: 30,
-        fill: {
-            color: "#E5A32B"
-        }
-    });
-    $('.skill-php').circleProgress({
-        value: 0.35,
-        size: 250,
-        thickness: 30,
-        fill: {
-            color: "#6785B7"
-        }
-    });
-    $('.skill-git').circleProgress({
-        value: 0.40,
-        size: 250,
-        thickness: 30,
-        fill: {
-            color: "#F34F29"
-        }
-    });
-
-
-    inited = true;
-  }
+el.appear({
+    force_process: true
 });
 
+el.on('appear', function() {
+    if (!inited) {
+
+        $('.skill-html5').circleProgress({
+            value: 0.85,
+            size: 250,
+            thickness: 30,
+            fill: {
+                color: "#E44D26"
+            }
+        });
+        $('.skill-css3').circleProgress({
+            value: 0.75,
+            size: 250,
+            thickness: 30,
+            fill: {
+                color: "#0171BB"
+            }
+        });
+        $('.skill-js').circleProgress({
+            value: 0.20,
+            size: 250,
+            thickness: 30,
+            fill: {
+                color: "#E5A32B"
+            }
+        });
+        $('.skill-php').circleProgress({
+            value: 0.35,
+            size: 250,
+            thickness: 30,
+            fill: {
+                color: "#6785B7"
+            }
+        });
+        $('.skill-git').circleProgress({
+            value: 0.40,
+            size: 250,
+            thickness: 30,
+            fill: {
+                color: "#F34F29"
+            }
+        });
+
+
+        inited = true;
+    }
+});
